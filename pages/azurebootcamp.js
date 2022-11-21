c2059055440c4226f6976b423dfdfa6a8e3ea62e
p3rcy7 <arijackson4@gmail.com>
2022-11-21 23:35:28 +0530

    azurebootcamp page footer component added

--- pages/azurebootcamp.js
+++ pages/azurebootcamp.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
-import ModalCompTerms from '../components/modal/Modal'
-import ModalCompRefund from '../components/modal/ModalRefund'
+import Footer from '../components/footer/footer'
 
 import NavBar from '../components/navbar/NavBar'
 
@@ -1154,55 +1153,7 @@ const AzureBootcamp = () => {
                 <div className={styles.enrollbutton}>Enroll Now</div>
             </div>
 
-            <div className={styles.footercontainer}>
-                <div className={styles.footerwrappers}>
-                    <div className={styles.footerwrapper}>
-                        <div className={styles.footerlinks}>
-                            <a className={styles.footerlink} href="">Home</a>
-                            <a className={styles.footerlink} href="">Services</a>
-                            <a className={styles.footerlink} href="">Solutions</a>
-                        </div>
-                        <div className={styles.footerlinks}>
-                            <a className={styles.footerlink} href="">Industries</a>
-                            <a className={styles.footerlink} href="">Who We Are</a>
-                            <a className={styles.footerlink} href="">Get in Touch</a>
-                        </div>
-
-                    </div>
-                    <div className={styles.footerdivider} />
-                    <div className={styles.footerwrapper}>
-                        <div className={styles.footerlinks}>
-                            <div className={styles.footerlink} href=""> <ModalCompRefund /> </div>
-                            <div className={styles.footerlink} href="">
-                                <ModalCompTerms /> </div>
-                            <a className={styles.footerlink} href="">Privacy Policy</a>
-                            <a className={styles.footerlink} href="">Disclaimer</a>
-                        </div>
-                        <div className={styles.footerlinks}>
-                            <a className={styles.footerlink} href="">Accessibilty</a>
-                            <a className={styles.footerlink} href="">Copyright Policy</a>
-                            <a className={styles.footerlink} href="">Customer Charter</a>
-                        </div>
-
-                    </div>
-                    <div className={styles.footerdivider} />
-                    <div className={styles.footertxt}>
-                        <div className={styles.footertxt1}>Stay Updated!
-                            🔥
-                        </div>
-                        <div className={styles.footertxt2}>Get updates on new programs, tips and snippets on
-                            the technology right in your inbox.
-                        </div>
-
-                    </div>
-                </div>
-                <div className={styles.inputgroup}>
-                    <input type="email" onClick={handleClick} onChange={e => { setEmail(e.target.value) }} className={styles.emailinput} value={email} id="Email" name="Email" autocomplete="off" />
-                    <div className={styles.buttonsubmit}  >
-                        <img src="tick.svg" alt="tick" />
-                    </div>
-                </div>
-            </div>
+            <Footer />
         </div>
     )
 }
